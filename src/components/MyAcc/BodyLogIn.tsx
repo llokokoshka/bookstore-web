54a34d025c92618be53527cb2c82af4e9740d5c9
Katerina <katelokota@gmail.com>
2024-10-28 14:30:16 +0300

    add connection woth server

--- src/components/MyAcc/BodyLogIn.tsx
+++ src/components/MyAcc/BodyLogIn.tsx
@@ -1,22 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
-import { useState } from 'react';
+import axios from 'axios';
+import { createAsyncThunk } from '@reduxjs/toolkit';
 
 import man from '../../img/чел 1.png';
 import mail from '../../img/Mail.png';
 import hide from '../../img/Hide.png';
+import { useAppDispatch } from '../../hooks';
 
 const LogIn: React.FC = () => {
+  const dispatch = useAppDispatch();
+
   const [inputType, setInputType] = useState('password');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
   const changeInputTypeHandler = () => {
     inputType === 'password' ? setInputType('text') : setInputType('password');
   };
 
+  const changeEmailValue = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+
+  const changePasswordValue = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
+  const loginUser = createAsyncThunk(
+    '/sign-in',
+    async ({ email, password }: { email: string; password: string }) => {
+      try {
+        const response = await axios.post('/auth/sign-in', {
+          email,
+          password,
+        });
+        console.log(response);
+        return response.data;
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  );
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    dispatch(loginUser({ email, password }));
+  };
+
   return (
     <StyledWrapper>
       <div className="poster__container">
-        <form method="post" className="container__info-block">
+        <form
+          method="post"
+          className="container__info-block"
+          onSubmit={handleSubmit}
+        >
           <div className="info-block__text">
             <div className="big-title">Log In</div>
             <div className="input">
@@ -25,6 +64,8 @@ const LogIn: React.FC = () => {
                 type="text"
                 placeholder="Email"
                 className="input__field"
+                value={email}
+                onChange={changeEmailValue}
               ></input>
             </div>
             <div>Enter your email</div>
@@ -39,8 +80,10 @@ const LogIn: React.FC = () => {
                 type={inputType}
                 placeholder="Password"
                 className="input__field"
-                pattern="[0-9a-fA-F]{4,8}"
+                // pattern="[0-9a-fA-F]{4,8}"
                 autoComplete="false"
+                value={password}
+                onChange={changePasswordValue}
               ></input>
             </div>
             <div>Enter your password</div>
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { ThemeProvider } from 'styled-components';
 import { RouterProvider } from 'react-router-dom';
+import axios from 'axios';
 
 import { store } from './store';
 import GlobalStyles from './styles/global';
@@ -10,6 +11,10 @@ import { baseTheme } from './styles/theme';
 import router from './routes/routes';
 
 const rootElem = document.getElementById('root');
+const token = '16ri7';
+axios.defaults.baseURL = 'http://localhost:4000';
+axios.defaults.headers.common['Authorization'] = `Bearer${token}`;
+axios.defaults.headers.post['Content-Type'] = 'application/json';
 
 if (rootElem) {
   const root = ReactDOM.createRoot(rootElem);
